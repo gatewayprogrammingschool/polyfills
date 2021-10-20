9e35f2b94df06f79a4f8128d2664e01938cc66a3
The Sharp Ninja <ninja@thesharp.ninja>
2021-10-20 07:49:17 -0500

    Checkpoint

---
+++ tests/Netstandard20.Range.Bridge.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Netstandard20.Range.Bridge.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
